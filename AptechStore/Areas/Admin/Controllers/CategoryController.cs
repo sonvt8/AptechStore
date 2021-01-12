55b8849dc4822ae253892dc899d73a91f9f2f211
Liem <leliem28@gmail.com>
2021-01-12 10:09:23 +0700

    Modify Category Controller and Views

--- AptechStore/Areas/Admin/Controllers/CategoryController.cs
+++ AptechStore/Areas/Admin/Controllers/CategoryController.cs
@@ -1,16 +1,54 @@
-﻿using System;
+﻿using AptechStore.DataAccess.Repositoty.IRepository;
+using AptechStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace AptechStore.Areas.Admin.Controllers
 {
-    public class CategoryController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	[Area("Admin")]
+	public class CategoryController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public CategoryController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Upsert(int? id)
+		{
+			Category category = new Category();
+			if (id == null)
+			{
+				// Create
+				return View(category);
+			}
+			// Edit
+			category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+
+		#region API CALLS
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			var allObj = _unitOfWork.Category.GetAll();
+			return Json(new { data = allObj });
+		}
+
+		#endregion
+	}
 }
