bfa58c63a46079a126775862ffbf88cfff4f4a9a
Liem <leliem28@gmail.com>
2021-01-12 11:03:45 +0700

    Add Upsert and Delete view for Category

--- AptechStore/Areas/Admin/Controllers/CategoryController.cs
+++ AptechStore/Areas/Admin/Controllers/CategoryController.cs
@@ -40,6 +40,26 @@ namespace AptechStore.Areas.Admin.Controllers
 			return View(category);
 		}
 
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Upsert(Category category)
+		{
+			if (ModelState.IsValid)
+			{
+				if (category.Id == 0)
+				{
+					_unitOfWork.Category.Add(category);
+				}
+				else
+				{
+					_unitOfWork.Category.Update(category);
+				}
+				_unitOfWork.Save();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(category);
+		}
+
 		#region API CALLS
 
 		[HttpGet]
@@ -49,6 +69,18 @@ namespace AptechStore.Areas.Admin.Controllers
 			return Json(new { data = allObj });
 		}
 
+		[HttpDelete]
+		public IActionResult Delete(int id)
+		{
+			var objFromDb = _unitOfWork.Category.Get(id);
+			if (objFromDb == null)
+			{
+				return Json(new { success = false, message = "Error while deleting" });
+			}
+			_unitOfWork.Category.Remove(objFromDb);
+			_unitOfWork.Save();
+			return Json(new { success = true, message = "Delete successfully" });
+		}
 		#endregion
 	}
 }
