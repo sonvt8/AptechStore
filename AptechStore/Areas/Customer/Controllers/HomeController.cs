c616f23d8928c24d6e6859c437f0958cb7b228d7
tin <tinsatthu1988@gmail.com>
2021-01-15 00:01:44 +0700

    Shopping Cart Refresh

--- AptechStore/Areas/Customer/Controllers/HomeController.cs
+++ AptechStore/Areas/Customer/Controllers/HomeController.cs
@@ -30,7 +30,17 @@ namespace AptechStore.Areas.Customer.Controllers
 		public IActionResult Index()
 		{
 			IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category");
-			return View(productList);
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            if (claim != null)
+            {
+                var count = _unitOfWork.ShoppingCart
+                    .GetAll(c => c.ApplicationUserId == claim.Value)
+                    .ToList().Count();
+
+                HttpContext.Session.SetInt32(SD.ssShoppingCart, count);
+            }
+            return View(productList);
 		}
 
 		public IActionResult Details(int id)
