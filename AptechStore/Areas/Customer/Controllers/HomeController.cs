d44e4041e39f8e0deba14c5b76f65c1cf1e2ee92
Liem <leliem28@gmail.com>
2021-01-14 09:45:06 +0700

    Add Product detail page

--- AptechStore/Areas/Customer/Controllers/HomeController.cs
+++ AptechStore/Areas/Customer/Controllers/HomeController.cs
@@ -11,33 +11,44 @@ using System.Threading.Tasks;
 
 namespace AptechStore.Areas.Customer.Controllers
 {
-    [Area("Customer")]
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private readonly IUnitOfWork _unitOfWork;
+	[Area("Customer")]
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
-        {
-            _logger = logger;
-            _unitOfWork = unitOfWork;
-        }
+		public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+		{
+			_logger = logger;
+			_unitOfWork = unitOfWork;
+		}
 
-        public IActionResult Index()
-        {
-            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category");
-            return View(productList);
-        }
+		public IActionResult Index()
+		{
+			IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category");
+			return View(productList);
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Details(int id)
+		{
+			var productFromDb = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category");
+			ShoppingCart cartObj = new ShoppingCart()
+			{
+				Product = productFromDb,
+				ProductId = productFromDb.Id
+			};
+			return View(cartObj);
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
